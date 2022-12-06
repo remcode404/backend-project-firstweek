4c72753958c7ae6fbaf5c2829a5a564a5ef890da
iznor <iznor.hucuev9507@gmail.comm>
2022-12-06 19:49:44 +0300

    доработал контроллер добавления продуктов

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,13 +3,14 @@ const Product = require("../models/Product.model");
 module.exports.productController = {
   createProduct: async (req, res) => {
     try {
-      const { name, price, img } = req.body;
+      const { name, price, img, categoryId } = req.body;
       const product = await Product.create({
         name,
         price,
         img,
+        categoryId,
       });
-      return res.json(product)
+      return res.json(product);
     } catch (e) {
       res.json({ e: e.message });
     }
@@ -27,7 +28,7 @@ module.exports.productController = {
   deleteProduct: async (req, res) => {
     try {
       await Product.findByIdAndDelete(req.params.id);
-    
+
       return res.json("DELETED");
     } catch (e) {
       res.json({ e: e.message });
