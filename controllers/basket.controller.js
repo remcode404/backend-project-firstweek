ff490015a2033cdfaa727d3158d9eea40da05941
malik <gaspar-2021@malik.ru>
2022-12-07 18:16:52 +0300

    ыф

--- controllers/basket.controller.js
+++ controllers/basket.controller.js
@@ -1,37 +1,33 @@
 const Basket = require("../models/Basket.model");
-const Product = require("../models/Product.model");
-const { findById } = require("../models/Users.model");
-const User = require("../models/Users.model");
+// const Product = require("../models/Product.model");
+// const { findById } = require("../models/Users.model");
+// const User = require("../models/Users.model");
 
 module.exports.basketController = {
-  // Добавление товара в корзину:
-  // addBasket: async (req, res) => {
-  //   try {
-  //     const { userId, productId } = req.body;
-  //     const data = await Basket.create({
-  //       userId,
-  //       productId,
-  //     });
-  //     res.json(data);
-  //   } catch (err) {
-  //     res.json(err);
-  //   }
-  // },
-
-  addBasket: async (req, res) => {
+  // ВЫВОД КОРЗИНЫ
+  getBusketByUser: async (req, res) => {
     try {
-      const { userId, productId } = req.body;
+      const data = await Basket.findOne({ userId: req.params.id });
+      return res.json(data);
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+  },
+  // ДОБАВЛЕНИЕ ТОВАРА В КОРЗИНУ
+  addProductToBasket: async (req, res) => {
+    try {
+      const { product } = req.body;
       const data = await Basket.findByIdAndUpdate(req.params.id, {
         $push: {
-          products: req.body.product,
+          products: product,
         },
       });
       res.json(data);
-    } catch (err) {
-      res.json(err);
+    } catch (error) {
+      res.json({ error: error.message });
     }
   },
-
+  // ИЗМЕНЕНИЕ СЧЕТЧИКА AMOUNT
   editAmount: async (req, res) => {
     try {
       // НАХОДИМ НАШУ КОРЗИНУ
@@ -58,83 +54,11 @@ module.exports.basketController = {
       await basket.updateOne({ products: newData });
 
       res.json(basket);
-    } catch (err) {
-      res.json(err);
+    } catch (error) {
+      res.json({ error: error.message });
     }
   },
-
-  // Изменение товара в корзине:
-  // editBasket: async (req, res) => {
-  //   const condidate = await User.findById(req.params.id);
-  //   const product = await Product.findById(req.body.productId);
-  //   const price = product.price;
-  //   try {
-  //     let isAdded = 0;
-  //     condidate.basket.map((item) => {
-  //       if (String(item.productId) === String(req.body.productId)) {
-  //         isAdded += 1;
-  //       }
-  //       return item;
-  //     });
-  //     if (req.body.type === "plus") {
-  //       if (isAdded) {
-  //         const newBasket = condidate.basket.map((item) => {
-  //           if (String(item.productId) === String(req.body.productId)) {
-  //             item.amount += 1;
-  //           }
-  //           return item;
-  //         });
-  //         await User.findByIdAndUpdate(
-  //           req.params.id,
-  //           { basket: newBasket },
-  //           { new: true }
-  //         );
-  //       } else {
-  //         await User.findByIdAndUpdate(
-  //           req.params.id,
-  //           {
-  //             $push: { basket: { productId: req.body.productId, amount: 1 } },
-  //           },
-  //           { new: true }
-  //         );
-  //       }
-
-  //       await User.findByIdAndUpdate(req.params.id, {
-  //         total: condidate.total + price,
-  //       });
-  //     }
-  //     if (req.body.type === "minus") {
-  //       if (isAdded) {
-  //         const newBasket = condidate.basket.map((item) => {
-  //           if (String(item.productId) === String(req.body.productId)) {
-  //             if (item.amount === 1) {
-  //               return null;
-  //             } else {
-  //               item.amount -= 1;
-  //             }
-  //           }
-  //           return item;
-  //         });
-  //         await User.findByIdAndUpdate(
-  //           req.params.id,
-  //           { basket: newBasket.filter((item) => item !== null) },
-  //           { new: true }
-  //         );
-  //         await User.findByIdAndUpdate(req.params.id, {
-  //           total: condidate.total - price,
-  //         });
-  //       } else {
-  //         return res.json({ error: "товара нет в корзине" });
-  //       }
-  //     }
-
-  //     const result = await User.findById(req.params.id);
-  //     res.json(result);
-  //   } catch (error) {
-  //     res.json({ error: error.message });
-  //   }
-  // },
-  // Удаление товара из корзины:
+  // УДАЛЕНИЕ ТОВАРА ИЗ КОРЗИНЫ
   deleteBasket: async (req, res) => {
     try {
       const data = await Basket.findByIdAndUpdate(req.params.id, {
@@ -143,8 +67,8 @@ module.exports.basketController = {
         },
       });
       res.json(data);
-    } catch (err) {
-      res.json(err);
+    } catch (error) {
+      res.json({ error: error.message });
     }
   },
 };
